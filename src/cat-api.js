2aebcc3a7a904d0711a622cf2a3a728c9baecc4e
Yuliia <djudit@ukr.net>
2023-12-05 18:11:53 +0200

    drop-down list done

---
+++ src/cat-api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_41ZLKc6w55aKAhNKcvazVK7pSDRpFwv5hmBkV3y3mU9I8HjFpMwjMfY85hbt4uCy';
+
+function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(resp => resp.data)
+    .catch(error => {
+      console.log(error);
+      throw new Error(err);
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(resp => resp.data)
+    .catch(error => {
+      console.log(error);
+      throw new Error(err);
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
---
+++ src/index.js
@@ -0,0 +1,86 @@
+import Notiflix from 'notiflix';
+
+const selectCat = document.querySelector('.breed-select');
+const loadCat = document.querySelector('.loader');
+const errorCat = document.querySelector('.error');
+const infoCat = document.querySelector('.cat-info');
+
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+selectCat.style.visibility = 'hidden';
+errorCat.style.display = 'none';
+
+fetchBreeds()
+  .then(breeds => {
+    selectCat.style.visibility = 'visible';
+    loadCat.style.visibility = 'hidden';
+    console.log(breeds);
+
+    const breedCats = breeds
+      .map(
+        breed => `
+    <option value="${breed.id}">${breed.name}</option>
+    `
+      )
+      .join('');
+
+    selectCat.insertAdjacentHTML('beforeend', breedCats);
+  })
+  .catch(err => {
+    loadCat.style.visibility = 'hidden';
+
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  });
+
+selectCat.addEventListener('change', handleChange);
+function handleChange(evt) {
+  const selectedBreedId = evt.value;
+
+  infoCat.innerHTML = '';
+
+  // loadCat.style.visibility = 'visible';
+  Notiflix.Notify.info('Loading data, please wait...');
+
+  fetchCatByBreed(selectedBreedId)
+    .then(breeds => {
+      loadCat.style.visibility = 'hidden';
+
+      const data = breeds[0];
+      console.log(data);
+      infoCat.innerHTML = `
+        <div><img src="${data.url}" alt="${data.breeds[0].name}" width ="600px"/></div>
+        <div>
+        <h3>${data.breeds[0].name}</h3>
+        <p>${data.breeds[0].description}</p>
+        <h4>Temperament</h4>
+        <p>${data.breeds[0].temperament}</p>
+        </div>
+        `;
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    });
+}
+// --------------------------------------------------------------------
+
+// infoCat.insertAdjacentHTML('beforeend', createMarkup(data.results));
+
+// function createMarkup(arr) {
+//   arr
+//     .map(
+//       ({ imageUrl, name, description, temperament }) => `
+//   <img src="${imageUrl}" alt="${name}" width="600px">
+//   <div>
+//        <h3>${name}</h3>
+//        <p>${description}</p>
+//        <h4>Temperament</h4>
+//        <p>${temperament}</p>
+//   </div>
+//   `
+//     )
+//     .join('');
+// }
